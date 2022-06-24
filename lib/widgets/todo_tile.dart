f0da1c84cfe217ddf65aa896a75ad65cfcfe4432
ggarber42 <ggarber@protonmail.com>
2022-06-23 23:45:48 -0300

    feat: add checkbox

--- lib/widgets/todo_tile.dart
+++ lib/widgets/todo_tile.dart
@@ -10,8 +10,21 @@ class TodoTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(10),
-      child: ListTile(
-        title: Text(todo.name as String)
+      child: Card(
+        child: ListTile(
+          title: Text(todo.name as String),
+          trailing: Checkbox(
+            checkColor: Colors.blue,
+            fillColor: MaterialStateProperty.resolveWith<Color>((states) {
+              if (states.contains(MaterialState.disabled)) {
+                return Colors.grey.withOpacity(.32);
+              }
+              return Colors.white38;
+            }),
+            value: todo.done as bool? true : false,
+            onChanged: (value) {},
+          ),
+        ),
       ),
     );
   }
