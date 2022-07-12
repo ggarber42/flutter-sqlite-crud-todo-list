be11ad7c26a03ea5cab66e2a0383fe5d221692da
ggarber42 <ggarber@protonmail.com>
2022-07-11 22:29:34 -0300

    feat: change checkbox at update

--- lib/widgets/todo_tile.dart
+++ lib/widgets/todo_tile.dart
@@ -3,16 +3,23 @@ import 'package:sqlite_crud/dao/todo_dao.dart';
 
 import '../models/todo_item.dart';
 
-class TodoTile extends StatelessWidget {
+class TodoTile extends StatefulWidget {
   final TodoItem todo;
-  final TodoDAO todoDAO = new TodoDAO();
 
   TodoTile(this.todo);
 
-  void _updateTodoItem() async{
-    todo.setDone = todo.getDone ==  0 ? 1 : 0;
-    var res = await todoDAO.update(todo);
-    print(res);
+  @override
+  _TodoTileState createState() => _TodoTileState();
+}
+
+class _TodoTileState extends State<TodoTile> {
+
+  void _updateTodoItem() async {
+    await TodoDAO().update(widget.todo);
+  }
+
+  int _toggleValue(int value){
+    return 1 - value;
   }
 
   @override
@@ -21,20 +28,25 @@ class TodoTile extends StatelessWidget {
       padding: EdgeInsets.all(10),
       child: Card(
         child: ListTile(
-          title: Text(todo.name),
-          trailing: Checkbox(
-            checkColor: Colors.white,
-            fillColor: MaterialStateProperty.resolveWith<Color>((states) {
-              if (states.contains(MaterialState.disabled)) {
-                return Colors.grey.withOpacity(.32);
-              }
-              return Colors.blue;
+            title: Text(widget.todo.name),
+            trailing: Checkbox(
+              checkColor: Colors.white,
+              fillColor: MaterialStateProperty.resolveWith<Color>((states) {
+                if (states.contains(MaterialState.disabled)) {
+                  return Colors.grey.withOpacity(.32);
+                }
+                return Colors.blue;
+              }),
+              value: widget.todo.done == 0 ? false : true,
+              onChanged: (value) {},
+            ),
+            onTap: () {
+              final newValue = _toggleValue(widget.todo.getDone);
+              setState(() {
+                  widget.todo.setDone = newValue;
+                _updateTodoItem();
+              });
             }),
-            value: todo.done== 0 ? false : true,
-            onChanged: (value) {},
-          ),
-          onTap: () => _updateTodoItem()
-        ),
       ),
     );
   }
