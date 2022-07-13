8a565b61e18366a7fefeba158cdc0123b4a088f5
ggarber42 <ggarber@protonmail.com>
2022-07-12 21:57:31 -0300

    refactor(ui): added red colors to delete dialog

--- lib/widgets/delete_dialog.dart
+++ lib/widgets/delete_dialog.dart
@@ -13,7 +13,6 @@ class DeleteDialog extends StatefulWidget {
 }
 
 class _DeleteDialogState extends State<DeleteDialog> {
-
   void _deleteTodoItem() async {
     Navigator.of(context).pop();
     await TodoDAO().delete(widget.todoId);
@@ -25,17 +24,26 @@ class _DeleteDialogState extends State<DeleteDialog> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: const Text('Are you sure?'),
+      title: const Text(
+        'Are you sure to delete?',
+        style: TextStyle(color: Colors.red),
+      ),
       actions: [
         TextButton(
           onPressed: () {
             Navigator.pop(context, 'No');
           },
-          child: const Text('No'),
+          child: const Text(
+            'No',
+            style: TextStyle(color: Colors.red),
+          ),
         ),
         TextButton(
           onPressed: () => _deleteTodoItem(),
-          child: const Text('Yes'),
+          child: const Text(
+            'Yes',
+            style: TextStyle(color: Colors.red),
+          ),
         ),
       ],
     );
